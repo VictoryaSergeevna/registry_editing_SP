456e743276adf9635888e2de243f80717b2d5811
VictoryaSergeevna <vikusyachk@gmail.com>
2020-05-29 17:24:36 +0300

    it's all

---
+++ registry_edit/SPDz_10/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace SPDz_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxKey = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxValue = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(23, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(375, 426);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(449, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Key";
+            // 
+            // textBoxKey
+            // 
+            this.textBoxKey.Location = new System.Drawing.Point(452, 37);
+            this.textBoxKey.Name = "textBoxKey";
+            this.textBoxKey.Size = new System.Drawing.Size(111, 20);
+            this.textBoxKey.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(449, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Value";
+            // 
+            // textBoxValue
+            // 
+            this.textBoxValue.Location = new System.Drawing.Point(452, 100);
+            this.textBoxValue.Name = "textBoxValue";
+            this.textBoxValue.Size = new System.Drawing.Size(111, 20);
+            this.textBoxValue.TabIndex = 9;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(468, 152);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 10;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(468, 204);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 11;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 450);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxValue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxKey);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxKey;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxValue;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
+    }
+}
+
---
+++ registry_edit/SPDz_10/Form1.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Windows.Forms;
+
+namespace SPDz_10
+{
+    public partial class Form1 : Form
+    {
+        string  path = @"Software\Microsoft\Windows\CurrentVersion\Run";
+        public Form1()
+        {
+            RegistryKey current = Registry.CurrentUser;
+            RegistryKey run = current.OpenSubKey(path, true);
+            InitializeComponent();
+            listView1.Columns.Add("Name");
+            listView1.Columns.Add("Value");
+            listView1.View = View.Details;
+            listView1.FullRowSelect = true;
+            foreach (var KeyName in run.GetValueNames())
+            {
+                ListViewItem lvi = new ListViewItem(KeyName);
+                lvi.SubItems.Add(run.GetValue(KeyName).ToString());
+                listView1.Items.Add(lvi);
+
+            }
+            current.Close();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+                RegistryKey current = Registry.CurrentUser;
+                RegistryKey run = current.OpenSubKey(path, true);
+                run.SetValue(textBoxKey.Text, textBoxValue.Text);
+                listView1.Items.Add(textBoxKey.Text + "  " + textBoxValue.Text);
+                current.Close();          
+           
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            RegistryKey current = Registry.CurrentUser;
+            RegistryKey run = current.OpenSubKey(path, true);
+            if (listView1.SelectedItems.Count == 1)
+            {
+                string tmp = listView1.SelectedItems[0].Text.ToString();
+                listView1.Items.RemoveAt(listView1.SelectedIndices[0]);
+                run.DeleteValue(tmp);
+            }
+            current.Close();
+        }
+       
+
+       
+    }
+}
